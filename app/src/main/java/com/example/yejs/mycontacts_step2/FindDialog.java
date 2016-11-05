4bd00bb825467af43bd553ea427befa88c5f6e3b
gdmec07150945 <960771292@qq.com>
2016-11-05 20:41:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yejs/mycontacts_step2/FindDialog.java
@@ -0,0 +1,13 @@
+package com.example.yejs.mycontacts_step2;
+
+/**
+ * Created by yejs on 2016/11/5.
+ */
+public class FindDialog {
+    public FindDialog(MainActivity mainActivity) {
+    }
+
+    public void show() {
+
+    }
+}
